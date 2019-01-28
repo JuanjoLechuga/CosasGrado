1f631cd8ab595736fefab0aa0e050adbc2eb607d
JuanjoLechuga <juanjo.chica.pablopicasso@gmail.com>
2019-01-28 08:49:19 +0100

    Primer commit del main

---
+++ src/calculadora/Main.java
@@ -0,0 +1,20 @@
+package calculadora;
+
+import java.util.Scanner;
+
+public class Main {
+
+	
+	private static int a,b;
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Introduzca el primer número a operar: ");
+		a = sc.nextInt();
+		System.out.print("Introduzca el segundo número a operar: ");
+		b =sc.nextInt();
+		
+
+	}
+
+}
